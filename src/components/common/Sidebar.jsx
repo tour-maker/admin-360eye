ac53a6f904f107bc3b8bd29c5dc32ab8076b86a1
Ubuntu <ubuntu@ip-172-31-33-164.eu-north-1.compute.internal>
2026-07-27 10:13:25 +0000

    fix: remove Partners sidebar link (build error) - keep route only, section renders on homepage

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -62,11 +62,6 @@ const Sidebar = () => {
       ]
     },
     {
-    {
-      title: "Partners",
-      icon: <FiUsers className="mr-3 text-lg" />,
-      path: "/partners",
-    },
       title: "Careers",
       icon: <FiUsers className="mr-3 text-lg" />,
       path: "/careers",
